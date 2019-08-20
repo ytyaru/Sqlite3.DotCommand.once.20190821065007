656ca1f29c7c8dc50331baf46812a399f1d00f7f
ytyaru <ytyaru@outlook.jp>
2019-08-21 06:54:31 +0900

    初回コミット

---
+++ src/run.sh
@@ -0,0 +1,25 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd "$SCRIPT_DIR"
+
+# -e: テキストエディタ（対話モード）
+#sqlite3
+#.once -e
+#select 1, 'AAA' union select 2, 'BBB';
+
+# -x: 表計算アプリ（対話モード）
+#sqlite3
+#.once -x
+#select 1, 'AAA' union select 2, 'BBB';
+
+# FILE: ファイル
+sqlite3 :memory: \
+".once a.csv" \
+"select 1, 'AAA' union select 2, 'BBB';" \
+".shell cat a.csv"
+cat a.csv
+
+# PIPE
+sqlite3 :memory: \
+".once '| xargs -I@ zenity --info --text \"@\"'" \
+"select 1, 'AAA' union select 2, 'BBB';"
+
